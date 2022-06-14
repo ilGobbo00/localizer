bcc18532413077c95985ee36b32ce6e23e76a0cb
Riccardo Gobbo <riccardogobbo00@gmail.com>
2022-06-14 18:26:43 +0200

    Comment

--- Localizer/app/src/main/java/it/unipd/localizer/database/SimpleLocationItem.kt
+++ Localizer/app/src/main/java/it/unipd/localizer/database/SimpleLocationItem.kt
@@ -1,4 +1,4 @@
 package it.unipd.localizer.database
 
-// Custom class to hold location main fields
+// Custom class to hold location main fields and easily converted with database converters
 class SimpleLocationItem(val latitude: Double, val longitude: Double, val  altitude: Double)
